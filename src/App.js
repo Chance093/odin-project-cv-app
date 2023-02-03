d161ce050dd0d6fb2cd5f45b474723acf7324e55
Chance093 <chance.dev093@gmail.com>
2023-02-03 02:51:02 -0800

    Add state with initial state changing functions

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
-function App() {
-  return (
-    <div className="App">
-      Hello World!
-    </div>
-  );
+import React, {Component} from 'react';
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      firstName: '',
+      lastName: '',
+      position: '',
+      address: '',
+      phoneNumber: '',
+      email: '',
+      description: '',
+      positionExp: '',
+      companyExp: '',
+      companyLocExp: '',
+      fromDateExp: '',
+      toDateExp: '',
+      descriptionExp: '',
+      schoolEdu: '',
+      locationEdu: '',
+      degreeEdu: '',
+      fieldEdu: '',
+      fromDateEdu: '',
+      toDateEdu: '',
+      descriptionEdu: '',
+      skills: '',
+    }
+  }
+
+  onFirstNameChange = (e) => {
+    this.setState({
+      firstName: e.target.value,
+    })
+  }
+
+  onLastNameChange = (e) => {
+    this.setState({
+      lastName: e.target.value,
+    })
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <input onChange={this.onFirstNameChange}></input>
+        <input onChange={this.onLastNameChange}></input>
+        <p>{this.state.firstName}</p>
+        <p>{this.state.lastName}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
