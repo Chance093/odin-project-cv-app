d66dea5383aa95c3b1e55c6bc34d2ecac94d7482
Chance093 <chance.dev093@gmail.com>
2023-02-05 14:29:04 -0800

    Add PersonalForm component

--- src/components/GeneralForm.js
+++ src/components/GeneralForm.js
@@ -1,13 +1,9 @@
+import PersonalForm from "./PersonalForm";
+
 function GeneralForm({onInputChange}){
     return(
         <div className="general-form">
-            <input type='text' placeholder="First Name" name='firstName' onChange={onInputChange}></input>
-            <input type='text' placeholder="Last Name" name='lastName' onChange={onInputChange}></input>
-            <input type='text' placeholder="Position" name='position' onChange={onInputChange}></input>
-            <input type='text' placeholder="Address" name='address' onChange={onInputChange}></input>
-            <input type='text' placeholder="Phone Number" name='phoneNumber' onChange={onInputChange}></input>
-            <input type='text' placeholder="Email" name='email' onChange={onInputChange}></input>
-            <input type='text' placeholder="Description" name='description' onChange={onInputChange}></input>
+            <PersonalForm onInputChange={onInputChange}/>
         </div>
     )
 }
---
+++ src/components/PersonalForm.js
@@ -0,0 +1,16 @@
+function PersonalForm({onInputChange}){
+    return(
+        <div className="personal-form">
+            <h2>Personal Information</h2>
+            <input type='text' placeholder="First Name" name='firstName' onChange={onInputChange}/>
+            <input type='text' placeholder="Last Name" name='lastName' onChange={onInputChange}/>
+            <input type='text' placeholder="Position" name='position' onChange={onInputChange}/>
+            <input type='text' placeholder="Address" name='address' onChange={onInputChange}/>
+            <input type='text' placeholder="Phone Number" name='phoneNumber' onChange={onInputChange}/>
+            <input type='text' placeholder="Email" name='email' onChange={onInputChange}/>
+            <input type='text' placeholder="Description" name='description' onChange={onInputChange}/>
+        </div>
+    )
+}
+
+export default PersonalForm;
