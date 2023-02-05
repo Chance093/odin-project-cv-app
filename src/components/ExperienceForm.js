c5424ca8aa785bf287ca98519b02b8b41201f499
Chance093 <chance.dev093@gmail.com>
2023-02-05 14:33:52 -0800

    Add ExperienceForm component

---
+++ src/components/ExperienceForm.js
@@ -0,0 +1,15 @@
+function ExperienceForm({onInputChange}){
+    return(
+        <div className="personal-form">
+            <h2>Experience</h2>
+            <input type='text' placeholder="Position" name='positionExp' onChange={onInputChange}/>
+            <input type='text' placeholder="Company" name='companyExp' onChange={onInputChange}/>
+            <input type='text' placeholder="City, State" name='companyLocExp' onChange={onInputChange}/>
+            <input type='text' placeholder="Start Date" name='fromDateExp' onChange={onInputChange}/>
+            <input type='text' placeholder="End Date" name='toDateExp' onChange={onInputChange}/>
+            <input type='text' placeholder="Description" name='descriptionExp' onChange={onInputChange}/>
+        </div>
+    )
+}
+
+export default ExperienceForm;
--- src/components/GeneralForm.js
+++ src/components/GeneralForm.js
@@ -1,9 +1,11 @@
+import ExperienceForm from "./ExperienceForm";
 import PersonalForm from "./PersonalForm";
 
 function GeneralForm({onInputChange}){
     return(
         <div className="general-form">
             <PersonalForm onInputChange={onInputChange}/>
+            <ExperienceForm onInputChange={onInputChange}/>
         </div>
     )
 }
