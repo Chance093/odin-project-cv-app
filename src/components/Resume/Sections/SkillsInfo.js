e22b50f82d5c17435af7e8ddd0be392c1af7afd0
Chance093 <chance.dev093@gmail.com>
2023-02-09 21:54:42 -0800

    Finish styling resume

--- src/components/Resume/Sections/SkillsInfo.js
+++ src/components/Resume/Sections/SkillsInfo.js
@@ -1,10 +1,10 @@
-import styles from '../Resume.module.css';
+import styles from './SkillsInfo.module.css';
 
 function SkillsInfo({ skills }) {
 	return (
-		<div className={styles.skillsInfo}>
-			<h1 className={styles.leftSkills}>Skills</h1>
-			<p className={styles.rightSkills}>{skills}</p>
+		<div className={styles.info}>
+			<h1 className={styles.left}>Skills</h1>
+			<p className={styles.right}>{skills}</p>
 		</div>
 	);
 }
